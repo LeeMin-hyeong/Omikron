f34374aba46b303452a096a6e168d6b296f4c630
LeeMin-hyeong <lmhstrar070@naver.com>
2023-09-20 21:48:50 +0900

    v1.2.0+ manual updater

--- updater.bat
+++ updater.bat
@@ -1,15 +1,31 @@
 @echo off
-echo Omikron Program Update Progress
+REM Step 1: Git 및 Python 3.10 설치
+echo Step 1: Installing Git and Python 3.10...
+choco install git -y
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+choco install python3 -y
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+python --version
 
-mkdir temp
+REM Step 2: 필요한 Python 패키지 설치
+echo Step 2: Installing required Python packages...
+pip install --upgrade openpyxl selenium webdriver-manager pyinstaller pywin32 python-dateutil
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-git clone -b v1.2.0 https://github.com/LeeMin-hyeong/Omikron.git temp
-pip install --upgrade webdriver-manager
-pip install --upgrade pyinstaller
-pip install --upgrade openpyxl
-pip install --upgrade selenium
+REM Step 3: Omikron 레포지토리 클론 및 빌드
+echo Step 3: Cloning Omikron repository and building omikrondb.py...
+git clone https://github.com/LeeMin-hyeong/Omikron.git
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+cd Omikron
+pyinstaller omikrondb.py
+IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cd temp
-pyinstaller -F omikrondb.py
-copy ./dist/omikrondb.exe ../
+REM 이동 및 삭제 작업 추가
+cd ..
+move Omikron\dist\* .
+rd /s /q Omikron
+
+REM 결과물 확인
+echo.
+echo Build completed. Check the current directory for the executable.
 pause
