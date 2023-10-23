5b3319d07a0d15a724c4f9ddc6ff00a7b08431b6
LeeMin-hyeong <lmhstrar070@naver.com>
2023-10-24 03:02:00 +0900

    manual updater update

--- updater.bat
+++ updater.bat
@@ -1,31 +1,27 @@
 @echo off
-REM Step 1: Git 및 Python 3.10 설치
-echo Step 1: Installing Git and Python 3.10...
-choco install git -y
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-choco install python3 -y
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+@chcp 65001 1> NUL 2> NUL
+
 python --version
+IF %ERRORLEVEL% NEQ 0 (
+    echo 수동 업데이트를 진행하기 위해서 python이 필요합니다.
+    pause
+    exit
+)
+git --version
+IF %ERRORLEVEL% NEQ 0 (
+    echo 수동 업데이트를 진행하기 위해서 git이 필요합니다.
+    pause
+    exit
+)
 
-REM Step 2: 필요한 Python 패키지 설치
-echo Step 2: Installing required Python packages...
 pip install --upgrade openpyxl selenium webdriver-manager pyinstaller pywin32 python-dateutil
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-REM Step 3: Omikron 레포지토리 클론 및 빌드
-echo Step 3: Cloning Omikron repository and building omikrondb.py...
 git clone https://github.com/LeeMin-hyeong/Omikron.git
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 cd Omikron
-pyinstaller omikrondb.py
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+pyinstaller -F omikrondb.py
 
-REM 이동 및 삭제 작업 추가
 cd ..
 move Omikron\dist\* .
 rd /s /q Omikron
 
-REM 결과물 확인
-echo.
-echo Build completed. Check the current directory for the executable.
+echo 수동 업데이트가 완료되었습니다.
 pause
